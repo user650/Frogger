aeedd74e8b6102dc7fa26f897fc4bb96c9382ba0
Scott Stubbs <scstubbs@pacbell.net>
2015-05-23 07:38:31 -0400

    Made corrections per the suggestions from the code review:
    > break aprat simple conditionals
    > seperate operators with a space
    > missing semi colons
    > fix indentation
    > make player step in full steps
    > fix column width = 101 (not /2)

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ var PLAYER_START_X = 200; // X where the player will start
 var PLAYER_START_Y = 400; // Y where the player will start
 var FIRST_ROW = 60; // Y position where the first enemy will start
 var ROW_HEIGHT = 83;// height of a row in pixels.  this was taken from the engine code
-var COL_WIDTH = 101/2; // width of the column as defined in the engine
-var STEP_SIZE = .5; // sets how big of a step the player will take each keystroke
+//var COL_WIDTH = 101; // width of the column as defined in the engine  (not currently used )
+var STEP_SIZE = 1; // sets how big of a step the player will take each keystroke
 var BOY = 'images/char-boy.png';
 var GIRL = 'images/char-princess-girl.png';
 
 
 //returns a random number between min and max
 function RandomInt(min, max) {
-  return Math.floor(Math.random() * (max+1 - min)) + min;
-};
+    return Math.floor(Math.random() * (max+1 - min)) + min;
+}
 
 /*places the player back at the start position
 resets the gems on the bugs
@@ -21,13 +21,13 @@ function Reset() {
     if (player.sex == 'female') {
         player.sprite = BOY;
         player.sex = 'male';
-    } else {
+    } 
+    else {
         player.sprite = GIRL;
-        player.sex = 'female'
+        player.sex = 'female';
     };
     player.x = PLAYER_START_X;   
     player.y = PLAYER_START_Y;
-
 }
 
 
@@ -45,17 +45,17 @@ var Enemy = function(x, y){
     this.greenCount = 0; // start out with no green gems
     this.blackCount = 0; // start out with no black gems
     this.direction = 1; // by default enemies ony go right unless their subclass allows for changing it
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // Reset the bugs that have drifted off of the screen either to the left or the right
     if (this.x > 700) { // the right direction gets reset to the left
-        this.x = -100
+        this.x = -100;
     };
     if (this.x < -100) { // the left direction bug gets reset to the right
-        this.x = 700
+        this.x = 700;
     };    
 
     // movement up and down occur when the bugs have a black gem count
@@ -64,8 +64,12 @@ Enemy.prototype.update = function(dt) {
         this.y = this.y + RandomInt(this.blackCount*-1, this.blackCount); 
 
         // keep the bug within the playing area
-        if (this.y < FIRST_ROW) {this.y = FIRST_ROW};  // if it staggers out of the lanes then put it back in bounds
-        if (this.y > FIRST_ROW + (ROW_HEIGHT*2)) {this.y = FIRST_ROW + ROW_HEIGHT*2};
+        if (this.y < FIRST_ROW) {
+            this.y = FIRST_ROW;
+        };  // if it staggers out of the lanes then put it back in bounds
+        if (this.y > FIRST_ROW + (ROW_HEIGHT*2)) {
+            this.y = FIRST_ROW + ROW_HEIGHT*2;
+        };
     };
 
     //movement left and right 
@@ -82,40 +86,40 @@ Enemy.prototype.update = function(dt) {
         greenGem.hide--;
     }
     else {
-        if (((greenGem.x <= (this.x + this.wide)) && ((greenGem.x + greenGem.wide) >= this.x)) && (((greenGem.y)<= this.y+this.tall) && ((greenGem.y+greenGem.tall)>=this.y))) {
+        if (((greenGem.x <= (this.x + this.wide)) && ((greenGem.x + greenGem.wide) >= this.x)) && (((greenGem.y) <= this.y+this.tall) && ((greenGem.y+greenGem.tall) >= this.y))) {
             if (this.greenCount < 5) {
                 this.greenCount++; // increment the green count if less than 5
-            } 
+            }; 
             greenGem.move();//move the gem
             
-            //assign the beg sprite based on the direction, how many green gems and if they have a duff  
-            // **todo this would be cleaner if you could augment the sprite with the color, duff, diretion rather than replace with a whole picture
+            // assign the bug sprite based on the direction, how many green gems and if they have a duff on their back  
+            // TODO: this would be cleaner if you could augment the sprite with the color, duff, diretion rather than replace with a whole picture
             if (this.greenCount === 1 ){
                 if (this.direction === -1) {
                     this.sprite = 'images/enemy-bug-left-wild1.png';
                 }
                 else {
                     this.sprite = 'images/enemy-bug-wild1.png';
-                }
-            }
+                };
+            };
             if (this.greenCount === 2 ){
                 if (this.direction === -1) {
                     this.sprite = 'images/enemy-bug-left-wild2.png';
                 }
                 else {
                     this.sprite = 'images/enemy-bug-wild2.png';
-                }
-            }
+                };
+            };
             if (this.greenCount === 3 ){
                 if (this.direction === -1) {
                     this.sprite = 'images/enemy-bug-left-wild3.png';
                 }
                 else {
                     this.sprite = 'images/enemy-bug-wild3.png';
-                }
-            }
-        }
-    }         
+                };
+            };
+        };
+    };    
  
     //  ------  bug hits a black gem ------
     /* add one to the number of black gems that the bugs collected
@@ -126,16 +130,16 @@ Enemy.prototype.update = function(dt) {
         blackGem.hide--;
     }
     else  {
-        if (((blackGem.x <= (this.x + this.wide)) && ((blackGem.x + blackGem.wide) >= this.x)) && (((blackGem.y)<= this.y+this.tall) && ((blackGem.y+blackGem.tall)>=this.y))) {
+        if (((blackGem.x <= (this.x + this.wide)) && ((blackGem.x + blackGem.wide) >= this.x)) && (((blackGem.y) <= this.y+this.tall) && ((blackGem.y+blackGem.tall) >= this.y))) {
             if (this.blackCount < 5) {
                 this.blackCount++;  // increment the black count if less than 5
-            }
+            };
             blackGem.move(); //move the gem
-        }
-    }
+        };
+    };
     // add the duff if the black gem count is greater than one
-    if (this.blackCount >=1) {
-        switch (this.sprite)    {
+    if (this.blackCount >= 1) {
+        switch (this.sprite) {
             case "images/enemy-bug.png":
                 this.sprite = "images/enemy-bug-duff.png";
             break;
@@ -160,8 +164,8 @@ Enemy.prototype.update = function(dt) {
             case "images/enemy-bug-left-wild3.png":
                 this.sprite = "images/enemy-bug-left-wild3-duff.png";
             break;
-        }
-    }
+        };
+    };
     
 
     //  ------  Player hits a bug  ------
@@ -169,9 +173,9 @@ Enemy.prototype.update = function(dt) {
     this if statement basically checks to see if rectangle defined as the enemy's body overlaps with the rectangle that defines the player (minus the head).
     the width and height of the enemy and the player is stored in the wide and tall.  The head height of the player is stored in head. */
     
-    if (((player.x <= (this.x + this.wide)) && ((player.x + player.wide) >= this.x)) && (((player.y+player.head)<= this.y+this.tall) && ((player.y+player.tall)>=this.y))){
+    if (((player.x <= (this.x + this.wide)) && ((player.x + player.wide) >= this.x)) && (((player.y + player.head) <= this.y + this.tall) && ((player.y + player.tall) >= this.y))){
         Reset();
-    }
+    };
 }
 
 // Draw the enemy on the screen, required method for game
@@ -191,7 +195,7 @@ var ladyBug = function(x, y, direction) {
     else { // default is to the right
         this.sprite = 'images/enemy-bug.png';
         this.direction = 1;
-    }
+    };
 };
 ladyBug.prototype = Object.create(Enemy.prototype);
 ladyBug.prototype.constructor = ladyBug;
@@ -201,64 +205,65 @@ ladyBug.prototype.constructor = ladyBug;
 var Player = function(sex) {
     if (sex == 'female') {
         this.sprite = 'images/char-princess-girl.png';
-        this.sex = 'female'
+        this.sex = 'female';
     }
     else {  // default to the boy
         this.sprite = 'images/char-boy.png';
-        this.sex = 'male'
-    }
+        this.sex = 'male';
+    };
     this.x = PLAYER_START_X;   // This sets the starting location for the player    
     this.y = PLAYER_START_Y;
     this.head = 35; // head size of player - this part can poke into enemy without collision
     this.tall = 60; // how tall the payer is
     this.wide = 70; // how wide the player is
-    this.score = [0,0] // boys score, girls score
+    this.score = [0,0]; // boys score, girls score
 };
 
 // This class requires an update(), 
 Player.prototype.update = function() {
-};
+}
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Player.handleInput() method. You don't need to modify this.
 Player.prototype.handleInput = function(key) { 
     switch(key) {
         case 'left': //only move left if there is room
-            this.x = this.x - (ROW_HEIGHT*STEP_SIZE);  // the step size is set at the constant section and a % of the row height
-          if (this.x < -1) {
+            this.x = this.x - (ROW_HEIGHT * STEP_SIZE);  // the step size is set at the constant section and a % of the row height
+            if (this.x < -1) {
                 this.x = -1;
             };
-            break;
+        break;
         case 'up':  // move up only if the feet are below the first row
             if (this.y + this.tall > FIRST_ROW) { 
-                this.y = this.y - (ROW_HEIGHT*STEP_SIZE);
-            } else { // if the player reached the top then score a point for either Boy or Girl then print the score
+                this.y = this.y - (ROW_HEIGHT * STEP_SIZE);
+            } 
+            else { // if the player reached the top then score a point for either Boy or Girl then print the score
                 if (player.sex == "female") {
                     player.score[1]++;
                     console.log("FEMALE point");
-                } else {
+                } 
+                else {
                     player.score[0]++;
                     console.log("MALE point");
-                }   
-                    // now print the score at the top of the sceen
-                    var canvas = document.querySelector('canvas');
-                    ctx.fillStyle = "white";
-                    ctx.fillRect(0,-20,510,70);  // clear out the previous score
-                    ctx.font = "36pt Impact";
-                    ctx.textAlign = "right";
-                    ctx.fillStyle = "white";
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 3;
-                    ctx.textAlign = "left";
-                    ctx.strokeText("Boys: "+player.score[0],0,40);
-                    ctx.textAlign = "right";
-                    ctx.strokeText("Girls: "+player.score[1],canvas.width,40);
-                Reset() // reset the screen
-            }
-
+                };   
+                // now print the score at the top of the sceen
+                var canvas = document.querySelector('canvas');
+                ctx.fillStyle = "white";
+                ctx.fillRect(0,-20,510,70);  // clear out the previous score
+                ctx.font = "36pt Impact";
+                ctx.textAlign = "right";
+                ctx.fillStyle = "white";
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 3;
+                ctx.textAlign = "left";
+                ctx.strokeText("Boys: "+player.score[0],0,40);
+                ctx.textAlign = "right";
+                ctx.strokeText("Girls: "+player.score[1],canvas.width,40);
+                Reset(); // reset the screen
+            };
             break;
         case 'right':
             this.x = this.x + (ROW_HEIGHT*STEP_SIZE);
@@ -272,7 +277,7 @@ Player.prototype.handleInput = function(key) {
             };
         default:
             break;
-    }   
+    };   
 }
 
 // Gem object creation.  Black gems make bugs go up and down; Green gems make bugs go faster
@@ -283,29 +288,29 @@ var Gem = function (x, y, tall, wide, image) {
     this.wide = wide;
     this.sprite = image;
     this.hide = 1000; // will be used for the number of renders to hide
-}
+};
 
 Gem.prototype.move = function (){
-    this.x = RandomInt(0,6)*this.wide;
-    this.y = FIRST_ROW + RandomInt(1,3)*this.tall;
+    this.x = RandomInt(0,6) * this.wide;
+    this.y = FIRST_ROW + RandomInt(1,3) * this.tall;
     this.hide = 1000; // after moveing hide the gem for this many renders
 }
 
 Gem.prototype.render = function (){
     if (this.hide === 0) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
+    };
 }
 
 // create green and black gems
-var greenGem = new Gem(ROW_HEIGHT*1.5, FIRST_ROW*2, 50, 50,'images/Gem Green.png');
-var blackGem = new Gem(ROW_HEIGHT*2.75, FIRST_ROW*2+ROW_HEIGHT,20,20,'images/Gem Duff.png');
+var greenGem = new Gem(ROW_HEIGHT * 1.5, FIRST_ROW * 2, 50, 50,'images/Gem Green.png');
+var blackGem = new Gem(ROW_HEIGHT * 2.75, FIRST_ROW * 2 + ROW_HEIGHT,20,20,'images/Gem Duff.png');
 
 // instantiate the lady bugs
 var lb1 = new ladyBug (100, FIRST_ROW);
 var lb2 = new ladyBug (-100, FIRST_ROW); // first row goes to the right
 var lb3 = new ladyBug (700, FIRST_ROW + ROW_HEIGHT,"left"); //second row goes to the left
-var lb4 = new ladyBug (-50, FIRST_ROW + (2*ROW_HEIGHT)); //third row goes to the right
+var lb4 = new ladyBug (-50, FIRST_ROW + (2 * ROW_HEIGHT)); //third row goes to the right
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [lb1, lb2, lb3, lb4];
@@ -313,9 +318,9 @@ var allEnemies = [lb1, lb2, lb3, lb4];
 // Place the player object in a variable called player be default the first player will be boy
 var player = new Player();
 
-/* **todo change th logic so there are physically two player objects, one for the male and one for the female.
-but then the render for the play will need to be changed to only paint the active player */
-//var girlPlayer = new Player('female')
+/* TODO: change th logic so there are physically two player objects, one for the male and one for the female.
+but then the render for the play will need to be changed to only paint the active player 
+var girlPlayer = new Player('female') */
 
 // This listens for key presses and sends the keys to your
 document.addEventListener('keyup', function(e) {
